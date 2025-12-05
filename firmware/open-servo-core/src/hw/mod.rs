06b25cb7ce8d0f2e64696ca7c2cde6a27b83ecb8
Aaron Qian <aq1018@gmail.com>
2025-12-05 01:12:15 -0800

    create stm32 hardware implementation and adapted main to use stm32hw module

---
+++ firmware/open-servo-core/src/hw/mod.rs
@@ -0,0 +1,4 @@
+pub mod trait_hw;
+pub mod adc_dma;
+
+pub use trait_hw::{Hw, UartPort};
--- firmware/open-servo-core/src/hw_stm32/mod.rs
+++ firmware/open-servo-core/src/hw_stm32/mod.rs
@@ -1,1 +1,5 @@
+pub mod hw_impl;
 pub mod init;
+
+pub use hw_impl::StmHw;
+pub use init::{init_adc, init_dma, init_gpio, init_rcc, init_tim1_pwm, init_tim2_counter};
