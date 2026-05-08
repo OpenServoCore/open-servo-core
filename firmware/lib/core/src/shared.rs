fb9ee70d10a9f694bb70dbb76578e803a491ffdd
Aaron Qian <aq1018@gmail.com>
2026-05-08 14:22:25 -0700

    defined Shared type to hold shared states between kernel and services

---
+++ firmware/lib/core/src/shared.rs
@@ -0,0 +1,18 @@
+//! Singleton state shared between kernel (ISR) and services (main loop).
+
+use crate::{ControlTable, StreamCoord};
+
+#[repr(C)]
+pub struct Shared {
+    pub table: ControlTable,
+    pub stream: StreamCoord,
+}
+
+impl Shared {
+    pub const fn const_new() -> Self {
+        Self {
+            table: ControlTable::const_new(),
+            stream: StreamCoord::const_new(),
+        }
+    }
+}
