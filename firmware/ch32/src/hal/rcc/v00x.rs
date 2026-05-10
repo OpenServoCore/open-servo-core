4fcdfe1d49629be40f66cfcdacc4c807c96ac98b
Aaron Qian <aq1018@gmail.com>
2026-05-09 22:57:35 -0700

    copy rcc hal from tinyboot-ch32, add rcc 48mhz init.

---
+++ firmware/ch32/src/hal/rcc/v00x.rs
@@ -0,0 +1,30 @@
+use ch32_metapac::{
+    FLASH, RCC,
+    rcc::vals::{Hpre, Pllsrc, Sw},
+};
+
+pub fn init_48mhz_hsi_pll() {
+    FLASH.actlr().modify(|w| w.set_latency(2));
+
+    RCC.cfgr0().modify(|w| {
+        w.set_pllsrc(Pllsrc::HSI);
+        w.set_hpre(Hpre::DIV1);
+    });
+
+    RCC.ctlr().modify(|w| w.set_pllon(true));
+    while !RCC.ctlr().read().pllrdy() {}
+
+    RCC.cfgr0().modify(|w| w.set_sw(Sw::PLL));
+    while RCC.cfgr0().read().sws() != Sw::PLL {}
+}
+
+#[inline]
+pub fn enable_gpio(port_index: usize) {
+    RCC.pb2pcenr()
+        .modify(|w| w.0 |= 1 << (2 + port_index));
+}
+
+#[inline]
+pub fn enable_afio() {
+    RCC.pb2pcenr().modify(|w| w.set_afioen(true));
+}
